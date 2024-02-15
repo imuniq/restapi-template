9b615fee9e9d1b32d901562864c66aad6b10399c
imuniq <147643889+imuniq@users.noreply.github.com>
2024-02-15 14:06:37 +0530

    added  objector class and param handler

--- Api/__init__.py
+++ Api/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from tinydb import TinyDB, Query
 from time import time
 import os
 import json
-
+import requests
+from .libs.requestHandler import Objector
 
 class Database:
     def __init__(self, db_files: list = []) -> None:
@@ -43,12 +44,82 @@ class APIHandler(Flask):
         self.db_endpoints = apidb.table("routes")
         self.db_visitors = logsdb.table("visitors")
         self.get_all_routes = self.db_endpoints.all()
+        self.http_request = requests.session()
 
     def clear_all_routes(self):
-     for i in self.db_endpoints.all():
-      return self.db_endpoints.remove(database.query.name == i['name'])
-     return True
+        """
+           Clear all routes from the database.
+        """
+        for i in self.db_endpoints.all():
+            return self.db_endpoints.remove(database.query.name == i['name'])
+        return True
+
+    def _set_params(self, method: str = "GET", params: list = []):
+        """
+        Set parameters for the API request.
+
+        Parameters:
+            - method (str): HTTP method (default: "GET").
+            - params (list): List of required parameters (default: []).
+
+        Returns:
+            - tuple: Tuple containing payload and error message.
+        """
+        supported_methods = ["GET", "POST"]
+        if method.upper() not in supported_methods:
+            return None, "Method Type Not Supported"
+
+        payload = request.args if method.upper() == "GET" else request.get_json()
+        _params = {}
+        for param in params:
+            if param not in payload:
+                return None, f"Required parameter '{param}' is missing..."
+            _params[param.strip()] = payload[param]
+
+        return Objector(_params), None
+
+    def _set_response(self,
+                      response: str | bytes | None = None,
+                      status: int | None = None,
+                      headers: dict | None = None,
+                      mimetype: str | None = None,
+                      content_type: str | None = None,
+                      direct_passthrough: bool = False) -> Response:
+        """
+        Set response attributes and create a Flask Response object.
+
+        Parameters:
+            - response (str | bytes | None): The response content.
+            - status (int | None): The response status code.
+            - headers (dict | None): Additional response headers.
+            - mimetype (str | None): The response MIME type.
+            - content_type (str | None): The response content type.
+            - direct_passthrough (bool): Whether to pass the response directly to the client.
+
+        Returns:
+            - Response: The Flask Response object.
+        """
+        return Response(
+            response=response,
+            status=status,
+            headers=headers,
+            mimetype=mimetype,
+            content_type=content_type,
+            direct_passthrough=direct_passthrough
+        )
     
+    def _jsonify(self, **json_data) -> Response:
+        """
+        Convert the provided data into a JSON response.
+
+        Parameters:
+            - **json_data: Keyword arguments containing the data to be converted into JSON.
+
+        Returns:
+            - Response: The JSON response object.
+        """
+        return jsonify(json_data)
+
     def register_route(self, name, info, methods=["GET"], anti_spam=0, params=[], route_private: bool = False):
         def decorator(func):
             route_data = {"name": name,
@@ -63,7 +134,8 @@ class APIHandler(Flask):
             else:
                 self.db_endpoints.upsert(route_data, self.query.name == name)
 
-            endpoint_name = f"{name}_endpoint"  # Generate a unique endpoint name based on the route name
+            # Generate a unique endpoint name based on the route name
+            endpoint_name = f"{name}_endpoint"
 
             @self.route("/" + name, methods=methods, endpoint=endpoint_name)
             def route_handler(*args, **kwargs):  # Use the generated unique endpoint name
@@ -76,7 +148,7 @@ class APIHandler(Flask):
                     if anti_spam_data:
                         time_passed = current_timestamp - \
                             anti_spam_data["last_used_timestamp"]
-                        
+
                         time_left = int(anti_spam - time_passed)
 
                         if time_passed < anti_spam and time_left != 0:
@@ -89,9 +161,6 @@ class APIHandler(Flask):
 
             return route_handler  # Return the generated route_handler function
         return decorator
-
-
-
 
 
 app = APIHandler(__name__)
